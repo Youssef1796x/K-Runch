a8e59a1636af50984e61e871ae5112cae9413cda
Youssef1796x <joo2007pro@gmail.com>
2026-09-16 19:30:21 +0300

    feat: map all menu images to menu items

--- src/data/menuImages.ts
+++ src/data/menuImages.ts
@@ -1,4 +1,5 @@
 export const menuItemImages: Record<string, string> = {
+  // كريب الدجاج
   "king-smoky": "/images/menu/photo_1_2026-09-16_18-14-04.jpg",
   "sonic-bomb-crepe": "/images/menu/photo_2_2026-09-16_18-14-04.jpg",
   "el-chico-fajita": "/images/menu/photo_3_2026-09-16_18-14-04.jpg",
@@ -6,16 +7,105 @@ export const menuItemImages: Record<string, string> = {
   "al-peccino": "/images/menu/photo_5_2026-09-16_18-14-04.jpg",
   "blue-wave-crepe": "/images/menu/photo_6_2026-09-16_18-14-04.jpg",
   "chicken-monster": "/images/menu/photo_7_2026-09-16_18-14-04.jpg",
+
+  // كريب اللحوم والأجبان
   "el-haty-mix": "/images/menu/photo_8_2026-09-16_18-14-04.jpg",
   "chili-bomb": "/images/menu/photo_9_2026-09-16_18-14-04.jpg",
   "cheese-mix": "/images/menu/photo_10_2026-09-16_18-14-04.jpg",
+
+  // كريب السي فود
   "titanic": "/images/menu/photo_11_2026-09-16_18-14-04.jpg",
   "tarh-el-bahr": "/images/menu/photo_12_2026-09-16_18-14-04.jpg",
+
+  // كريب الحلو
   "nutella-bomb": "/images/menu/photo_13_2026-09-16_18-14-04.jpg",
   "nutella-banana": "/images/menu/photo_14_2026-09-16_18-14-04.jpg",
   "nutty-bomb-candy": "/images/menu/photo_15_2026-09-16_18-14-04.jpg",
+
+  // سندوتشات الفراخ
   "golden-strips": "/images/menu/photo_16_2026-09-16_18-14-04.jpg",
   "blue-wave-sandwich": "/images/menu/photo_17_2026-09-16_18-14-04.jpg",
   "sonic-bomb-sandwich": "/images/menu/photo_18_2026-09-16_18-14-04.jpg",
   "fire-shot": "/images/menu/photo_19_2026-09-16_18-14-04.jpg",
+  "shish-pop": "/images/menu/photo_20_2026-09-16_18-14-04.jpg",
+  "buffalo-bomb": "/images/menu/photo_21_2026-09-16_18-14-04.jpg",
+  "fajita-class": "/images/menu/photo_22_2026-09-16_18-14-04.jpg",
+
+  // تشيكن برجر
+  "chicken-hill": "/images/menu/photo_23_2026-09-16_18-14-04.jpg",
+  "buffalo-strips": "/images/menu/photo_24_2026-09-16_18-14-04.jpg",
+  "mr-sour": "/images/menu/photo_25_2026-09-16_18-14-04.jpg",
+
+  // بيف برجر
+  "cheesy-lazy": "/images/menu/photo_26_2026-09-16_18-14-04.jpg",
+  "classic-combo": "/images/menu/photo_27_2026-09-16_18-14-04.jpg",
+  "cowboy": "/images/menu/photo_28_2026-09-16_18-14-04.jpg",
+  "mushroom-lava": "/images/menu/photo_29_2026-09-16_18-14-04.jpg",
+
+  // سماش برجر
+  "smash-town": "/images/menu/photo_30_2026-09-16_18-14-04.jpg",
+  "smash-hunter": "/images/menu/photo_31_2026-09-16_18-14-04.jpg",
+  "smoky-mushroom": "/images/menu/photo_32_2026-09-16_18-14-04.jpg",
+
+  // وجبات السي فود
+  "hala-bel-khamees": "/images/menu/photo_33_2026-09-16_18-14-04.jpg",
+  "ibn-hamido": "/images/menu/photo_34_2026-09-16_18-14-04.jpg",
+  "al-sahel-al-shereer": "/images/menu/photo_35_2026-09-16_18-14-04.jpg",
+
+  // سندوتشات السي فود
+  "bahreya": "/images/menu/photo_36_2026-09-16_18-14-04.jpg",
+  "zafir": "/images/menu/photo_37_2026-09-16_18-14-05.jpg",
+  "balbaa": "/images/menu/photo_38_2026-09-16_18-14-05.jpg",
+  "golden-shrimp": "/images/menu/photo_39_2026-09-16_18-14-05.jpg",
+  "al-lol": "/images/menu/photo_40_2026-09-16_18-14-05.jpg",
+  "gaddorah": "/images/menu/photo_41_2026-09-16_18-14-05.jpg",
+  "apache-seafood": "/images/menu/photo_42_2026-09-16_18-14-05.jpg",
+
+  // باستا ميلانو
+  "negresco-lava": "/images/menu/photo_43_2026-09-16_18-14-05.jpg",
+  "crunchy-white": "/images/menu/photo_44_2026-09-16_18-14-05.jpg",
+  "white-lava": "/images/menu/photo_45_2026-09-16_18-14-05.jpg",
+  "sicilia": "/images/menu/photo_46_2026-09-16_18-14-05.jpg",
+  "venezia": "/images/menu/photo_47_2026-09-16_18-14-05.jpg",
+  "honey-moon": "/images/menu/photo_48_2026-09-16_18-14-05.jpg",
+  "roma-meat": "/images/menu/photo_49_2026-09-16_18-14-05.jpg",
+  "bolognese": "/images/menu/photo_50_2026-09-16_18-14-05.jpg",
+  "toscani": "/images/menu/photo_51_2026-09-16_18-14-05.jpg",
+
+  // قسم البطاطس
+  "cheetos-potato": "/images/menu/photo_52_2026-09-16_18-14-05.jpg",
+  "twins": "/images/menu/photo_53_2026-09-16_18-14-05.jpg",
+  "mix-sauce": "/images/menu/photo_54_2026-09-16_18-14-05.jpg",
+  "lava-buffalo": "/images/menu/photo_55_2026-09-16_18-14-05.jpg",
+  "flamingo": "/images/menu/photo_56_2026-09-16_18-14-05.jpg",
+  "extreme": "/images/menu/photo_57_2026-09-16_18-14-05.jpg",
+
+  // قسم الحرش
+  "kebda": "/images/menu/photo_58_2026-09-16_18-14-05.jpg",
+  "sogoq": "/images/menu/photo_59_2026-09-16_18-14-05.jpg",
+  "hawawshi": "/images/menu/photo_60_2026-09-16_18-14-05.jpg",
+  "mokh-al-prince": "/images/menu/photo_61_2026-09-16_18-14-05.jpg",
+  "mombar-sandwich": "/images/menu/photo_62_2026-09-16_18-14-05.jpg",
+
+  // وجبات الحرش
+  "kings-meal": "/images/menu/photo_63_2026-09-16_18-14-05.jpg",
+  "candy-beef": "/images/menu/photo_64_2026-09-16_18-14-05.jpg",
+  "kokoroko": "/images/menu/photo_65_2026-09-16_18-14-05.jpg",
+
+  // المشروبات
+  "pepsi-cans": "/images/menu/photo_66_2026-09-16_18-14-05.jpg",
+  "free-cola": "/images/menu/photo_67_2026-09-16_18-14-05.jpg",
+  "twist-classic": "/images/menu/photo_68_2026-09-16_18-14-05.jpg",
+  "pepsi-1-5l": "/images/menu/photo_69_2026-09-16_18-14-05.jpg",
+  "almarai-rayeb": "/images/menu/photo_70_2026-09-16_18-14-05.jpg",
+  "big-cola": "/images/menu/photo_71_2026-09-16_18-14-05.jpg",
+  "aswan-hibiscus": "/images/menu/photo_72_2026-09-16_18-14-05.jpg",
+  "sobia-milk": "/images/menu/photo_73_2026-09-16_18-14-05.jpg",
+  "tamarind": "/images/menu/photo_74_2026-09-16_18-14-05.jpg",
+  "kharoub": "/images/menu/photo_75_2026-09-16_18-14-05.jpg",
+  "sakans-mix": "/images/menu/photo_76_2026-09-16_18-14-05.jpg",
+
+  // وجبات اللحوم والدجاج
+  "kofta-sogoq-meal": "/images/menu/photo_77_2026-09-16_18-14-05.jpg",
+  "shish-tawook-meal": "/images/menu/photo_78_2026-09-16_18-14-05.jpg",
 };
