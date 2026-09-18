63973514621e7db0396a53900baee8c6b82beeb9
Youssef1796x <joo2007pro@gmail.com>
2026-09-18 15:55:42 +0300

    Allow Wikimedia tamarind image host

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "m.media-amazon.com",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
     ],
   },
 };
