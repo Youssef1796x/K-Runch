7282ad3234477725c9e7477af2c855caba8e93b7
Youssef1796x <joo2007pro@gmail.com>
2026-09-18 15:55:38 +0300

    Use mapped real photo for tamarind

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -865,7 +865,7 @@ export const menuItems: MenuItem[] = [
     category: "drinks",
     name: "تمر هندي أصلي",
     price: 25,
-    image: "/images/menu/tamarind-original.svg",
+    image: "",
     available: true,
   },
   {
