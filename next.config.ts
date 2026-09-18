0042cb4001311aa8ff399a2bf0429a2351e5b0a0
Youssef1796x <joo2007pro@gmail.com>
2026-09-18 15:59:46 +0300

    Allow tamarind photo host

--- next.config.ts
+++ next.config.ts
@@ -48,6 +48,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "upload.wikimedia.org",
       },
+      {
+        protocol: "https",
+        hostname: "snapcalorie-webflow-website.s3.us-east-2.amazonaws.com",
+      },
     ],
   },
 };
