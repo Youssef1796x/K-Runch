fde5d00c77e0aebc6ae1682e69fbece50c34a3d1
Yossef1796x <joo2007pro@gmail.com>
2026-09-19 15:36:59 +0300

    goooo

--- src/data/menuImages.ts
+++ src/data/menuImages.ts
@@ -63,12 +63,12 @@ export const menuItemImages: Record<string, string> = {
 
   // باستا ميلانو
   "negresco-lava": "https://images.pexels.com/photos/37336685/pexels-photo-37336685.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "crunchy-white": "https://images.pexels.com/photos/9731722/pexels-photo-9731722.jpeg?auto=compress&cs=tinysrgb&w=800",
+  "crunchy-white": "/images/menu/pasta1.jpg",
   "white-lava": "https://images.pexels.com/photos/35622190/pexels-photo-35622190.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "sicilia": "https://images.pexels.com/photos/19842780/pexels-photo-19842780.jpeg?auto=compress&cs=tinysrgb&w=800",
+  "sicilia": "/images/menu/pasta2.jpg",
   "venezia": "https://images.pexels.com/photos/16525900/pexels-photo-16525900.jpeg?auto=compress&cs=tinysrgb&w=800",
   "honey-moon": "https://images.pexels.com/photos/31235402/pexels-photo-31235402.jpeg?auto=compress&cs=tinysrgb&w=800",
-  "roma-meat": "https://images.pexels.com/photos/33673991/pexels-photo-33673991.jpeg?auto=compress&cs=tinysrgb&w=800",
+  "roma-meat": "/images/menu/pasta3.jpg",
   "bolognese": "https://images.pexels.com/photos/15500452/pexels-photo-15500452.jpeg?auto=compress&cs=tinysrgb&w=800",
   "toscani": "https://images.pexels.com/photos/17346205/pexels-photo-17346205.jpeg?auto=compress&cs=tinysrgb&w=800",
 
